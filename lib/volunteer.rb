636fe26bd30634713f6fe7876e6a0b3cb57f91f4
Nate Cottle <cottle.nate@gmail.com>
2018-07-13 09:42:35 -0700

    Add 2nd, 3rd, and 4th passing test for Volunteer.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,4 +7,25 @@ class Volunteer
     @id = attr.fetch(:id)
   end
 
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each do |volunteer|
+      name = volunteer['name']
+      project_id = volunteer.fetch('project_id').to_i
+      id = volunteer['id'].to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
+  def save
+    results = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = results.first['id']
+  end
+
+  def ==(another_volunteer)
+    self.name.==(another_volunteer.name).&self.project_id.==(another_volunteer.project_id)
+  end
+
 end
