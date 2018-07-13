40dceccc7027b3d25508d1a71433e3d59bce4f7c
Nate Cottle <cottle.nate@gmail.com>
2018-07-13 10:44:08 -0700

    add starting routes in app.rb and html in views files.

--- app.rb
+++ app.rb
@@ -6,3 +6,33 @@ require('./lib/project')
 require('./lib/volunteer')
 require('pg')
 require('pry')
+
+DB = PG.connect({:dbname => "volunteer_tracker_test"})
+
+get('/') do
+  @projects = Project.all
+  @volunteers = Volunteer.all
+  erb(:index)
+end
+
+post('/new_project') do
+  title = params.fetch("title")
+  project = Project.new({:title => title, :id => nil})
+  project.save
+  redirect back
+end
+
+post('/new_volunteer') do
+  name = params.fetch("name")
+  project_id = params.fetch("project_id")
+  volunteer = Volunteer.new({:name => name, :project_id => project_id, :id => nil})
+  volunteer.save
+  redirect back
+end
+
+get('/project/:id') do
+  id = params[:id].to_i
+  @project = Project.find(id)
+  @volunteers = @project.volunteers
+  erb(:project)
+end
