01042252cf3b5731dc5eb1eb5e0d45e9d2658def
Nate Cottle <cottle.nate@gmail.com>
2018-07-13 09:52:18 -0700

    All specs passing for Project class.

--- lib/project.rb
+++ lib/project.rb
@@ -38,6 +38,18 @@ def delete
   DB.exec("DELETE FROM projects WHERE id = #{self.id};")
 end
 
+def volunteers
+  project_volunteers = []
+  volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{self.id};")
+  volunteers.each do |volunteer|
+    name = volunteer['name']
+    project_id = volunteer['project_id'].to_i
+    id = volunteer['id'].to_i
+    project_volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+  end
+  project_volunteers
+end
+
 def ==(another_project)
   self.title.==(another_project.title)
 end
