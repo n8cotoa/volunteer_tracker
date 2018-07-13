a7ac744b488d2ee1fd902355f39735eb4958d520
Nate Cottle <cottle.nate@gmail.com>
2018-07-13 11:24:18 -0700

    Add third passing integration test.

--- spec/volunteer_integration_spec.rb
+++ spec/volunteer_integration_spec.rb
@@ -6,9 +6,6 @@ require('spec_helper')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# Your project should be set up so that a volunteer can only be created if a project already exists. (This makes it easier to assign the one to many relationship in Sinatra.) Focus on getting one integration spec passing at a time.
-
-# The user should be able to visit the home page and fill out a form to add a new project. When that project is created, the application should direct them back to the homepage.
 describe 'the project creation path', {:type => :feature} do
   it 'takes the user to the homepage where they can create a project' do
     visit '/'
@@ -21,5 +18,15 @@ describe 'the project creation path', {:type => :feature} do
     expect(page).to have_content('Please create a project to add volunteers')
   end
 end
-
+describe 'project detail path', {:type => :feature} do
+  it 'creates a project, goes to the project, and updates project title' do
+    visit '/'
+    fill_in('title', :with => 'Teaching Kids to Code')
+    click_button('Add Project')
+    click_link('Teaching Kids to Code')
+    fill_in('new_title', :with => 'Teaching Kids to Cook')
+    click_button('Update')
+    expect(page).to have_content('Teaching Kids to Cook')
+  end
+end
 # A user should be able to click on a project to see its detail. The detail page includes a form where the project can be updated. When the form is submitted, the user can be directed to either the home page or that project's detail page. (The test will work for either.)
